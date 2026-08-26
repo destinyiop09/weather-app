e2e2b92b66a33086f3e24378f57ebf348f697427
Emmanuel Aidoo <emmanuelaidoo2005@gmail.com>
2026-08-26 03:33:27 +0000

    api_base url has been changed

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // FastAPI backend URL
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = 'https://weather-app-iy3u.onrender.com';
 
 
 // ==================================================
